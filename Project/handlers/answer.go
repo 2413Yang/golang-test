f521e7904b07be2b51dc47d628368fb16dcb5744
yangshuai <yangshuai@192.168.0.162>
2023-04-10 18:19:50 +0800

    gin+gorm

---
+++ Project/handlers/answer.go
@@ -0,0 +1,16 @@
+package handlers
+
+import (
+	"net/http"
+	"strconv"
+
+	"github.com/gin-contrib/sessions"
+	"github.com/gin-gonic/gin"
+	"github.com/jinzhu/gorm"
+	"questions/pkg/database"
+	"questions/pkg/models"
+)
+
+func SaveAnswer(c *gin.Context) {
+	
+}
